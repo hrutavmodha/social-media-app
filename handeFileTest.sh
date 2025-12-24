98f8abe63a987b85420ff2887d8764796e19255d
Hrutav Modha <modhahrutav@gmail.com>
2025-12-24 17:30:23 +0530

    Post commit cleanup

--- handeFileTest.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST http://localhost:3000/posts \
-    -H "Cookie: token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IkhydXRhdiBNb2RoYSIsImVtYWlsIjoibW9kaGFocnV0YXZAZ21haWwuY29tIiwicGFzc3dvcmQiOiIkMmIkMTAkZll5d1RPOGhDaEN2U0Y0Rk43NGZ4dUJGVlhBdUEyckVuaTBPSUlsWDlRMzJhZVNBbDhaQW0iLCJjcmVhdGVkYXQiOiIyMDI1LTEyLTIyVDA3OjE2OjA1LjExN1oiLCJpYXQiOjE3NjY0MTI5Njd9.QqCPGqfbBLLzq2CXz56WCKulOce3xdJI4q5ab32ccUA" \
-    -F "media=@./package.json" \
-    -F "caption=Hello from Curl"
-
-# 
